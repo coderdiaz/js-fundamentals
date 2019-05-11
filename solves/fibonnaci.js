6f76d885fea83e950385b861f19857c95010063a
Javier Diaz <hello@coderdiaz.me>
2019-05-11 13:40:27 -0500

    fix: fixed fibonacci

--- solves/fibonnaci.js
+++ solves/fibonnaci.js
@@ -1,10 +1,14 @@
 // Fibonacci
 var fibonacci = function(n) {
-  var total = 0;
-  for(var index = 1; index <= n; index++) {
-    total += index;
+  var a = 0, b = 1, f = 1;
+  for(var index = 2; index <= n; index++) {
+    f = a + b;
+    a = b;
+    b = f;
   }
-  return total;
+  return f;
 }
 
-console.log(fibonnaci(5));
+for(var index = 1; index <= 100; index++) {
+  console.log(fibonacci(index));
+}
